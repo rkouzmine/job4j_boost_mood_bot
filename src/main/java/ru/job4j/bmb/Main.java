70bb6eeae39e8828b020a87abaf177ef20ba8bea
rkouzmine <krrsea@gmail.com>
2025-10-08 16:10:45 +0300

    Настройка окружения *.properties

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import ru.job4j.bmb.content.Content;
-import ru.job4j.bmb.services.TelegramBotService;
 
 @SpringBootApplication
 public class Main {
@@ -15,10 +13,9 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner initTelegramApi(ApplicationContext ctx) {
+    public CommandLineRunner checkEnv(ApplicationContext ctx) {
         return args -> {
-            var bot = ctx.getBean(TelegramBotService.class);
-            bot.receive(new Content());
+            System.out.println(ctx.getEnvironment().getProperty("telegram.bot.name"));
         };
     }
 }
