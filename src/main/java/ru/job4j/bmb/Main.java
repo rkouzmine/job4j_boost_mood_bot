2e574a2f5dd5b85120bebb619cb814506eac5167
rkouzmine <krrsea@gmail.com>
2025-10-09 09:59:44 +0300

    Интеграция Telegram API

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -5,17 +5,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.job4j.bmb.services.TgRemoteService;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
     @Bean
-    public CommandLineRunner checkEnv(ApplicationContext ctx) {
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println(ctx.getEnvironment().getProperty("telegram.bot.name"));
+            var bot = ctx.getBean(TgRemoteService.class);
+            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            try {
+                botsApi.registerBot(bot);
+                System.out.println("Бот успешно зарегистрирован");
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         };
     }
 }
--- src/main/java/ru/job4j/bmb/MoodBotApplication.java
+++ src/main/java/ru/job4j/bmb/MoodBotApplication.java
@@ -2,12 +2,10 @@ package ru.job4j.bmb;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import ru.job4j.bmb.config.AppConfig;
 
-@SpringBootApplication
 public class MoodBotApplication {
 
     public static void main(String[] args) {
