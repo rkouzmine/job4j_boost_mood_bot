79b319c15138edf5901584c40cc116efd0734330
rkouzmine <krrsea@gmail.com>
2025-11-17 14:48:33 +0300

    MoodService - бизнес логика системы

--- src/main/java/ru/job4j/bmb/Main.java
+++ src/main/java/ru/job4j/bmb/Main.java
@@ -3,14 +3,19 @@ package ru.job4j.bmb;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.job4j.bmb.model.Award;
 import ru.job4j.bmb.model.Mood;
 import ru.job4j.bmb.model.MoodContent;
 import ru.job4j.bmb.repository.AwardRepository;
 import ru.job4j.bmb.repository.MoodContentRepository;
 import ru.job4j.bmb.repository.MoodRepository;
+import ru.job4j.bmb.services.TgRemoteService;
 
 import java.util.ArrayList;
 
@@ -20,6 +25,20 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            var bot = ctx.getBean(TgRemoteService.class);
+            var botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            try {
+                botsApi.registerBot(bot);
+                System.out.println("Бот успешно зарегистрирован");
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        };
     }
 
     @Bean
--- src/main/java/ru/job4j/bmb/services/MoodService.java
+++ src/main/java/ru/job4j/bmb/services/MoodService.java
@@ -2,13 +2,15 @@ package ru.job4j.bmb.services;
 
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.model.Achievement;
+import ru.job4j.bmb.model.Mood;
 import ru.job4j.bmb.model.MoodLog;
 import ru.job4j.bmb.model.User;
 import ru.job4j.bmb.repository.AchievementRepository;
 import ru.job4j.bmb.repository.MoodLogRepository;
 import ru.job4j.bmb.repository.UserRepository;
-import ru.job4j.bmb.services.RecommendationEngine;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -36,16 +38,39 @@ public class MoodService {
     }
 
     public Content chooseMood(User user, Long moodId) {
+        Mood mood = new Mood();
+        mood.setId(moodId);
+        MoodLog moodLog = new MoodLog();
+        moodLog.setUser(user);
+        moodLog.setMood(mood);
+        moodLog.setCreatedAt(Instant.now().getEpochSecond());
+
+        moodLogRepository.save(moodLog);
+
         return recommendationEngine.recommendFor(user.getChatId(), moodId);
     }
 
     public Optional<Content> weekMoodLogCommand(long chatId, Long clientId) {
+        Instant now = Instant.now();
+        long weekAgo = now.minus(Duration.ofDays(7)).getEpochSecond();
+        var logs = moodLogRepository.findAll().stream()
+                .filter(user -> user.getUser().getId().equals(clientId))
+                .filter(log -> log.getCreatedAt() >= weekAgo)
+                .toList();
         var content = new Content(chatId);
+        content.setText(formatMoodLogs(logs, "Лог настроений за неделю"));
         return Optional.of(content);
     }
 
     public Optional<Content> monthMoodLogCommand(long chatId, Long clientId) {
+        Instant now = Instant.now();
+        long monthAgo = now.minus(Duration.ofDays(30)).getEpochSecond();
+        var logs = moodLogRepository.findAll().stream()
+                .filter(user -> user.getUser().getId().equals(clientId))
+                .filter(log -> log.getCreatedAt() >= monthAgo)
+                .toList();
         var content = new Content(chatId);
+        content.setText(formatMoodLogs(logs, "Лог настроений за месяц"));
         return Optional.of(content);
     }
 
@@ -62,7 +87,15 @@ public class MoodService {
     }
 
     public Optional<Content> awards(long chatId, Long clientId) {
+        var achievementAwards = achievementRepository.findAll().stream()
+                .filter(user -> user.getUser().getId().equals(clientId))
+                .toList();
         var content = new Content(chatId);
+        StringBuilder sb = new StringBuilder();
+        for (Achievement a : achievementAwards) {
+            sb.append(a.getAward().getDescription()).append("\n");
+        }
+        content.setText(sb.toString());
         return Optional.of(content);
     }
 }
