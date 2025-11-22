9c38e308d7d9aac18bb487da7198d4d69286a793
rkouzmine <krrsea@gmail.com>
2025-11-22 22:48:25 +0300

    ApplicationListener | AchievementService

--- src/main/java/ru/job4j/bmb/services/AchievementService.java
+++ src/main/java/ru/job4j/bmb/services/AchievementService.java
@@ -3,21 +3,31 @@ package ru.job4j.bmb.services;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.job4j.bmb.content.Content;
 import ru.job4j.bmb.events.UserEvent;
+import ru.job4j.bmb.model.Achievement;
+import ru.job4j.bmb.model.Award;
 import ru.job4j.bmb.repository.AchievementRepository;
+import ru.job4j.bmb.repository.AwardRepository;
 import ru.job4j.bmb.repository.MoodLogRepository;
+
+import java.time.Instant;
+import java.util.Optional;
 
 @Service
 public class AchievementService implements ApplicationListener<UserEvent> {
     private final AchievementRepository achievementRepository;
     private final MoodLogRepository moodLogRepository;
+    private final AwardRepository awardRepository;
     private final SentContent sentContent;
 
     public AchievementService(AchievementRepository achievementRepository,
                               MoodLogRepository moodLogRepository,
+                              AwardRepository awardRepository,
                               SentContent sentContent) {
         this.achievementRepository = achievementRepository;
         this.moodLogRepository = moodLogRepository;
+        this.awardRepository = awardRepository;
         this.sentContent = sentContent;
     }
 
@@ -25,5 +35,24 @@ public class AchievementService implements ApplicationListener<UserEvent> {
     @Override
     public void onApplicationEvent(UserEvent event) {
         var user = event.getUser();
+        int markedDays = moodLogRepository.findByUserId(user.getId()).size();
+
+        Optional<Award> award = awardRepository.findAll().stream()
+                .filter(a -> a.getDays() == markedDays)
+                .findFirst();
+
+        Content content = new Content(user.getChatId());
+
+        if (award.isEmpty()) {
+            content.setText("У Вас нет наград 😟");
+        } else {
+            Achievement achievement = new Achievement();
+            achievement.setUser(user);
+            achievement.setAward(award.get());
+            achievement.setCreateAt(Instant.now().getEpochSecond());
+            achievementRepository.save(achievement);
+            content.setText("🎉 Поздравляем! Вы получили награду: " + award.get().getDescription());
+        }
+        sentContent.sent(content);
     }
 }
